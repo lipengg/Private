e1cddee2ebe11c3c02cbc1f1b2163b0506242fce
lipengg <lipengg@163.com>
2021-12-10 22:25:25 +0800

    【功能】添加底部导航栏 1.添加底部照片和视频导航栏

--- app/src/main/java/com/example/album/MainActivity.kt
+++ app/src/main/java/com/example/album/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.album
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.viewmodel.MainViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     //var viewModel: MainViewModel? = null
+    private var TAG = "MainActivity"
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +25,20 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, it, Toast.LENGTH_LONG).show()
             }
         })
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav.setOnNavigationItemSelectedListener{item->
+            when (item.itemId) {
+                R.id.bottom_nav_image -> {
+                    Log.e(TAG, "open image page: ")
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.bottom_nav_video -> {
+                    Log.e(TAG, "open video page: ")
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
