64e3872f827f482a7bb98f16d68e34788aed89a2
lipengg <lipengg@163.com>
2021-12-11 21:42:55 +0800

    【功能】添加回收站页面

--- app/src/main/java/com/example/album/MainActivity.kt
+++ app/src/main/java/com/example/album/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.album
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.viewmodel.MainViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,11 +37,25 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.bottom_nav_image -> {
                     Log.e(TAG, "open image page: ")
+                    findNavController(R.id.nav_main_fragment).navigate(R.id.mainFragment)
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.bottom_nav_video -> {
                     Log.e(TAG, "open video page: ")
+                    findNavController(R.id.nav_main_fragment).navigate(R.id.mainFragment)
                     return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+        val leftNav = findViewById<NavigationView>(R.id.left_nav_view)
+        leftNav.setNavigationItemSelectedListener { item->
+            when (item.itemId) {
+                R.id.recycleFragment -> {
+                    findNavController(R.id.nav_main_fragment).navigate(R.id.recycleFragment)
+                    findViewById<DrawerLayout>(R.id.draw_layout).closeDrawer(Gravity.LEFT)
+                    return@setNavigationItemSelectedListener true
                 }
             }
             false
