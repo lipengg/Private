fc57b104e92d2b0205933ab57de995a4b882e941
lipengg <lipengg@163.com>
2022-01-23 12:19:57 +0800

    【功能】视频播放添加横竖屏自动适配

--- app/src/main/java/com/example/album/activity/VideoActivity.kt
+++ app/src/main/java/com/example/album/activity/VideoActivity.kt
@@ -1,5 +1,7 @@
 package com.example.album.activity
 
+import android.content.pm.ActivityInfo
+import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -16,24 +18,48 @@ class VideoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        this.window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
         setContentView(R.layout.activity_video)
+
+
+
+        //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+
+//        if(this.getResources().getConfiguration().orientation ==Configuration.ORIENTATION_PORTRAIT){
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
 
         intent?.extras?.let{
             val path = it.getString("path")
             try {
                 path?.let {
-                    Log.i(TAG, "video path $path")
+                    Log.i(TAG, "video path $path origin path ")
+
+                    val uri: Uri = Uri.parse(path)
+
+                    var retr = MediaMetadataRetriever();
+                    retr.setDataSource(this , uri);
+                    var wVideo = retr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
+                    var hVideo = retr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
+
+                    Log.i(TAG, "video resolution $wVideo*$hVideo")
+                    Log.i(TAG, "before video requestedOrientation $requestedOrientation")
+                    if(hVideo.toInt() > wVideo.toInt()) {
+                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                    }
+
+                    Log.i(TAG, "after video requestedOrientation $requestedOrientation")
+
+
                     //Creating MediaController
                     val mediaController = MediaController(this, false)
                     mediaController.setAnchorView(videoView_activity)
                     mediaController.setMediaPlayer(videoView_activity)
                     //specify the location of media file
-                    val uri: Uri = Uri.parse(path)
                     //Setting MediaController and URI, then starting the videoView
                     videoView_activity.setMediaController(mediaController)
                     videoView_activity.setVideoURI(uri)
